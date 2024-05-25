369f51a706956d7570ea97c1362cf5b5fbf588e9
AMALENDU MANOJ <220229@tkmce.ac.in>
2024-05-25 19:31:45 +0530

    deployed to sepolia test

--- Context/context.js
+++ Context/context.js
@@ -1,3 +1,3 @@
 import tasks from "../Context/Tasks.json"
-export const TaskAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const TaskAddress = "0x9F3Ff035E85C9f5af309Ce84C3875E5f8C57e7aD";
 export const TaskAbi = tasks.abi;
